41129c5ec4ed1786eb01fe66f358f4e882ce18f5
askarakshabayev <askar.akshabayev@gmail.com>
2018-08-30 10:06:22 +0600

    added week 1

---
+++ week1/1.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <cmath>
+#include <vector>
+
+using namespace std;
+
+int main() {
+    //  cout << "hello world" << '\n' << "abc" << "dfadf";
+    // cout << "hello world" << endl;
+    // cout << "abc";
+    cout << "5 + 6" << " " << 2 * 3;
+    return 0;
+    cout << "hello!!!";
+}
